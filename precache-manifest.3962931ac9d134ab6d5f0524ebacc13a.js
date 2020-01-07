b9f70d379b166357429755f86256b65a98ded6f5
yasmi <yasmi.zhamborova@mail.ru>
2020-01-07 21:00:55 +0300

    Updates

---
+++ precache-manifest.3962931ac9d134ab6d5f0524ebacc13a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf706e1a8ab5dc8879e32cb2b9350c7",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "697b0c3678becec4476c",
+    "url": "/monsters/static/css/main.ee36bb7f.chunk.css"
+  },
+  {
+    "revision": "b9a05f7033303c8aa2f8",
+    "url": "/monsters/static/js/2.6d8213e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.6d8213e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "697b0c3678becec4476c",
+    "url": "/monsters/static/js/main.9be6c15a.chunk.js"
+  },
+  {
+    "revision": "fcc61893b7a9935ddc3e",
+    "url": "/monsters/static/js/runtime-main.9faa307c.js"
+  }
+]);
